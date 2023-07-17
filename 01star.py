d1f594f3f237544b83ebe0af5aad5ebe989be663
yukihoshino <yukihoshinostudio@gmail.com>
2023-07-17 18:45:42 -0400

    star

---
+++ 01star.py
@@ -0,0 +1,24 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+import turtle
+turtle.fd(100)
+turtle.rt(144)
+turtle.fd(100)
+>>> turtle.rt(144)
+... 
+>>> 
+>>> turtle.fd(100)
+>>> turtle.rt(144)
+>>> turtle.fd(100)
+>>> turtle.rt(144)
+... turtle.fd(100)
+SyntaxError: multiple statements found while compiling a single statement
+>>> 
+>>> turtle.rt(144)
+>>> turtle.fd(100)
+>>> turtle.setposition(100,100)
+>>> turtle.circle(100)
+>>> turtle.undo()
+turtle.u
+>>> 
+>>> turtle.undo()
