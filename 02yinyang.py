fd123f3d91b794bf8e331b4fa7a67ba90fd63a5e
yukihoshino <yukihoshinostudio@gmail.com>
2023-07-21 17:18:19 -0400

    02

---
+++ 02yinyang.py
@@ -0,0 +1,70 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import.turle
+SyntaxError: invalid syntax
+>>> import turtle
+>>> turtle.circle(200,?)
+SyntaxError: invalid syntax
+>>> turtle.circle(200,90)
+>>> turtle.circle(200,120)
+>>> turtle.circle(200,360)
+>>> import turtle
+>>> turtle.shape("turtle")
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    turtle.shape("turtle")
+  File "<string>", line 5, in shape
+turtle.Terminator
+>>> turtle.circle(200)
+>>> turtle.shape("turtle")
+>>> turtle.circle(100,180)
+>>> turtle.(-100,180)
+SyntaxError: invalid syntax
+>>> turtle.(-100,180)
+SyntaxError: invalid syntax
+>>> turtle.circle(-100,180)
+>>> turtle.right(90)
+>>> turtle.penup
+<function penup at 0x000001DF1D83D580>
+>>> turtle.penup
+... 
+<function penup at 0x000001DF1D83D580>
+
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+
+
+
+
+
+
+
+
+
+
+turtle.penup()
+turte.forward(50)
+Traceback (most recent call last):
+  File "<pyshell#36>", line 1, in <module>
+    turte.forward(50)
+NameError: name 'turte' is not defined. Did you mean: 'turtle'?
+turtle.forward(50)
+turtle.right(90)
+turtle.circle(50)
+turtle.circle(50)
+turtle.pendown()
+turtle.circle(50)
+turtle.left(90)
+turtle.penup()
+turtle.forward(100)
+turtle.forward(100)
+
+turtle.right(90)
+turtle.pendown()
+turtle.circle(50)
