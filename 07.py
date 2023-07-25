63f97a3e1de41888a7cd6b3d271d338222432ba6
yukihoshino <yukihoshinostudio@gmail.com>
2023-07-24 20:23:31 -0400

    push 07

--- 07.py
+++ 07.py
@@ -2,12 +2,36 @@ import turtle
 # adjust pen size
 turtle.pensize(3)
 turtle.shape("turtle")
-#paintcircle
+
+#outer
+turtle.penup()
+turtle.goto(325,0)
+turtle.left(90)  
+turtle.pendown()
+turtle.color("red")
+
+turtle.begin_fill()
+turtle.circle(325)
+turtle.end_fill()
+
+turtle.right(90)
+
+
+#paint circle
 turtle.penup()
 turtle.goto(175,0)
 turtle.left(90)
 turtle.pendown()
+turtle.color("blue")
+
+turtle.begin_fill()
 turtle.circle(175)
+turtle.end_fill()
+
+
+#star
+turtle.color("white")
+turtle.begin_fill()
 turtle.penup()
 turtle.goto(-166,54)
 turtle.right(90)
@@ -15,19 +39,14 @@ turtle.pendown()
 turtle.forward(333)
 turtle.right(144)
 turtle.forward(333)
-turtle.right(90)
-turtle.undo()
-turtle.undo()
-turtle.forward(333)
 turtle.right(144)
 turtle.forward(333)
 turtle.right(144)
 turtle.forward(333)
 turtle.right(144)
 turtle.forward(333)
-turtle.penup()
-turtle.goto(175,0)  
-turtle.goto(325.0)
-# keep screen open
+turtle.right(144)
+
+turtle.end_fill()
 
 
