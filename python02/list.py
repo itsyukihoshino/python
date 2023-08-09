a69be2c15d5b2f957020f77ab15e61668421c424
itsyukikohoshino <139811280+itsyukihoshino@users.noreply.github.com>
2023-08-09 12:02:17 -0500

    update

---
+++ python02/list.py
@@ -0,0 +1,7 @@
+#declare a list
+my_abc = ["a","b","c"]
+print(my_abc)
+
+my_number = [1,2,3,4,5,6,7,8]
+print(my_number)      
+
---
+++ python02/list02.py
@@ -0,0 +1,47 @@
+# change items in a list
+
+fruits = ["apple", "banana", "grapes"]
+print(fruits)
+
+fruits[1] ="strawberry"
+
+print(fruits)
+
+fruits[2] ="orange"   
+
+print(fruits)
+
+
+# adding items to a list
+colors = ["yellow", "purple"]
+
+colors.append("red")
+colors.append("green")
+colors.append("yellow")
+print(colors)
+
+#joining lists 
+
+sedan = ["toyota camry", "honda Accord","Nissan sentra"]
+Trucks = ["Toyota tundra", "Ford F-150", "chevy silverado "]
+
+vehicles = sedan + Trucks 
+print(vehicles)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
