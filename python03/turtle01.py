bac4836bcac48bd5a595b640c1fdf06199581a96
itsyukikohoshino <139811280+itsyukihoshino@users.noreply.github.com>
2023-08-10 14:24:58 -0500

    Update turtle01.py

--- python03/turtle01.py
+++ python03/turtle01.py
@@ -1,17 +1,50 @@
 import turtle
 
+#create and setup turtle window
 win = turtle.Screen()
 win.setup(800,600)
 # win.bgpic('grid.gif')
 win.title('Example 20 ')
 
+#create turtle
 t1 = turtle.Turtle()
 t1.speed("slowest")
-t1.pencolor("blue")           
+t1.pencolor("blue")
+t1.pensize(5)
+
+#draw turtle
+t1.penup()
+t1.goto(100,100)
+t1.pendown()
+t1.goto(-100,100)
+t1.goto(-100,-100)
+t1.goto(100,-100)
+t1.goto(100,100)
+t1.circle(50)
+t1.right(180)
+t1.penup()
+t1.forward(200)
+t1.pendown()
+t1.right(90)
+t1.circle(50)
+t1.left(180)
+t1.forward(50)
+t1.left(90)
+t1.penup()
+t1.forward(66)
+t1.left(90)
+t1.pendown()
+t1.circle(15)
+t1.right(90)
+t1.penup()
+t1.forward(50)
+t1.pendown()
+t1.circle(15)
 
 
 
 
 
 
-turtle.exitonclick()
+
+win.exitonclick()
