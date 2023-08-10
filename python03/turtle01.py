ce3595f5091f861625771a6966a74dcfe0fae02b
itsyukikohoshino <139811280+itsyukihoshino@users.noreply.github.com>
2023-08-10 11:19:35 -0500

    update

---
+++ python03/turtle01.py
@@ -0,0 +1,17 @@
+import turtle
+
+win = turtle.Screen()
+win.setup(800,600)
+# win.bgpic('grid.gif')
+win.title('Example 20 ')
+
+t1 = turtle.Turtle()
+t1.speed("slowest")
+t1.pencolor("blue")           
+
+
+
+
+
+
+turtle.exitonclick()
