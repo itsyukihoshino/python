86dbadbd73ab075dff4fbdbe74a9a170208f4574
itsyukikohoshino <139811280+itsyukihoshino@users.noreply.github.com>
2023-08-13 17:00:42 -0500

    Update Example_30.py

---
+++ python03/Example_30.py
@@ -0,0 +1,29 @@
+import turtle
+
+win = turtle.Screen()
+win.setup(800,600)
+win.bgpic('grid.gif')
+win.title('Example 30')
+
+t1 = turtle.Turtle()
+t1.speed("slow")
+t1.pencolor("light green")
+t1.pensize(6)
+
+t1.forward(100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
