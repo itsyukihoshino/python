a186a2c335d9b40a2180c12a40bd998629b7df60
itsyukikohoshino <139811280+itsyukihoshino@users.noreply.github.com>
2023-08-13 22:54:37 -0500

    Update Example_30.py

--- python03/Example_30.py
+++ python03/Example_30.py
@@ -2,7 +2,7 @@ import turtle
 
 win = turtle.Screen()
 win.setup(800,600)
-win.bgpic('grid.gif')
+# win.bgpic('grid.gif')
 win.title('Example 30')
 
 t1 = turtle.Turtle()
@@ -10,20 +10,11 @@ t1.speed("slow")
 t1.pencolor("light green")
 t1.pensize(6)
 
+t1.left(90)
 t1.forward(100)
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
+turtle.exitonclick()
