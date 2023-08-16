110f40799b9d2712708b64d21558ecab93dea07e
itsyukikohoshino <139811280+itsyukihoshino@users.noreply.github.com>
2023-08-16 17:22:32 -0500

    rockpaperscissorgame

---
+++ python04/game.py
@@ -0,0 +1,35 @@
+import random 
+
+user_wins = 0
+computer_wins = 0
+choices = ["rock", "paper", "scissors"]                                 
+
+while True:
+    user_input = input("Type rock/paper/scissors or Q to quit:").lower()
+
+    if user_input == "q":
+        break
+    if user_input not in choices:
+        print("Invalid input. Please choose rock, paper, or scissors.")
+        continue
+    
+
+    random_number = random.randint(0, 2)
+    computer_pick = choices[random_number]
+    print("Computer picked", computer_pick)
+
+    if (user_input == "rock" and computer_pick == "scissors") or \
+    (user_input == "paper" and computer_pick == "rock") or \
+    (user_input == "scissors" and computer_pick == "paper"):
+        print("You won!")
+        user_wins += 1
+    elif user_input == computer_pick:
+        print("It's a tie!")
+    else:
+        print("You lost!")
+        computer_wins += 1
+
+# Print the game results after the loop ends
+print("You won", user_wins, "times.")
+print("The computer won", computer_wins, "times.")
+print("Goodbye!")
