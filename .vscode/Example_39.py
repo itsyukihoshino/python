b9e472fb8b04c8fc4837490dae7d108c57bc67dd
itsyukikohoshino <139811280+itsyukihoshino@users.noreply.github.com>
2023-08-18 15:23:59 -0500

    w

---
+++ .vscode/Example_39.py
@@ -0,0 +1,12 @@
+import turtle
+turtle.pencolor("light blue")
+turtle.pensize(7)
+turtle.shape("turtle")
+turtle.right(140)
+turtle.forward(300)
+turtle.left(140)
+turtle.forward(300)
+turtle.left(140)
+turtle.forward(100)
+
+turtle.exitonclick()
---
+++ python03/Example_40.py
@@ -0,0 +1,28 @@
+import turtle
+turtle.pensize(7)
+turtle.bgcolor("light blue")
+turtle.pencolor("white")
+turtle.circle(189)
+turtle.left(180)
+turtle.circle(269)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+turtle.exitonclick()
