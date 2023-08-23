dbb910fd90dfd3e460fe0dde6e2c9eb0d3a319b6
itsyukikohoshino <139811280+itsyukihoshino@users.noreply.github.com>
2023-08-23 09:49:01 -0500

    Create challange_23.py

---
+++ python04/challange_23.py
@@ -0,0 +1,14 @@
+import turtle
+
+win = turtle.Screen()
+win.setup(800, 600)
+win.title('challenge 23 - Draw your Own Emoji')
+
+t1 = turtle.Turtle()
+t1.speed("fast")
+t1.pencolor("yellow")
+t1.pensize(3)
+
+t1.penup()
+t1.goto(0, -150)
+turtle.exitonclick()
