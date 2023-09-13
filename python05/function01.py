5c71ced233ba26d4d0948097a560d651808a398d
itsyukikohoshino <139811280+itsyukihoshino@users.noreply.github.com>
2023-09-13 14:34:46 -0500

    Update function01.py

---
+++ python05/function01.py
@@ -0,0 +1,11 @@
+def hello_there(firstname, lastname):
+    print(f"Hello {firstname} {lastname}!")
+    
+def addition(a, b):
+    print(a + b)
+
+def division(a, b):
+    print(a / b)
+
+# hello_there("yukiko", "Hoshino")
+# division(2,4)
