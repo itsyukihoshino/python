562e19d183ab97924170a4a1e4e43a1e4fa19b0c
itsyukikohoshino <139811280+itsyukihoshino@users.noreply.github.com>
2023-09-14 13:49:15 -0500

    Update function04.py

--- python05/function04.py
+++ python05/function04.py
@@ -3,15 +3,15 @@ import turtle
 
 def draw_star(x, y, color):
     t = turtle.turtles
-    t.speed("fastest")
-    t.penup()
-    t.goto(x,y)
-    t.pensize(10)
-    t.pencolor(color)
-    t.pendown()
+    turtle.speed("fastest")
+    turtle.penup()
+    turtle.goto(x,y)
+    turtle.pensize(10)
+    turtle.pencolor(color)
+    turtle.pendown()
     for i in range(80):
-        t.forward(i)
-        t.left(144)
+        turtle.forward(i)
+        turtle.left(144)
 
 
 window = turtle.Screen()
@@ -19,11 +19,15 @@ window.setup(800,600)
 window.title('function 04')
 
 
-draw_star(-100,100,"red") #top left corner
-draw_star(-100,-100,"blue") #bottom left corner
+draw_star(-100,100,"light pink") #top left corner
+draw_star(-100,-100,"light blue") #bottom left corner
+draw_star(100, -100,"light green")
+draw_star(100,100,"light yellow")
+draw_star(0,0,"purle")
 
 
 
 
+window.exitonclick()
 
 
