1523c124a7f19847068baaf48af9197dd9139ab4
itsyukikohoshino <139811280+itsyukihoshino@users.noreply.github.com>
2023-09-15 19:00:11 -0500

    Update SpaceGame-Step1.py

--- game1/SpaceGame-Step1.py
+++ game1/SpaceGame-Step1.py
@@ -5,3 +5,11 @@ win.title("SPACE SHIP VS WITH UFOS")
 win.setup(800, 600)
 
 win.bgpic("space-bg.gif")
+
+#register graphic with turtle graphics
+turtle.register_("ship.gif")
+
+#create a turtle for my ship
+spaceship = turtle.Turtle()
+spaceship.shape("ship.gif")
+
