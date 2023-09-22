8f45bb8ee2f64c6811e2d06528c2b2f06f51f241
itsyukikohoshino <139811280+itsyukihoshino@users.noreply.github.com>
2023-09-22 08:30:44 -0500

    f

--- game1/SpaceGame-Step6.py
+++ game1/SpaceGame-Step6.py
@@ -7,12 +7,22 @@ def left():
     
 def right():
     global moveShipBy
-    moveShipBy = 3  
+    moveShipBy = 3
     
+def space():
+    global bullet
+    global spaceship
+    
+    if bullet.isvisibl() == False:
+        bullet.goto(spaceship.xcor(),spaceship.ycor)
+        bullet.showturtle   
+        
+        
 #enable and register keyboard events
 turtle.listen()
-turtle.onkey(left,"Left")
-turtle.onkey(right,"Right")
+turtle.onkey(left, "Left")
+turtle.onkey(right, "Right")
+turtle.onkey(space, "space")
 
 win = turtle.Screen()
 win.title("SPACE SHIP Vs UFOS!")
@@ -22,6 +32,7 @@ win.tracer(0)
 
 #register graphic with turtle graphics
 turtle.register_shape("ship.gif")
+turtle.register_shape("bullet.gif")
 
 #create a turtle for my ship
 spaceship = turtle.Turtle()
@@ -30,13 +41,20 @@ spaceship.penup()
 spaceship.speed(0)
 spaceship.goto(0, -200)
 
+#create a turtle for my bullet laser blast
+bullet = turtle.Turtle()
+bullet.hideturtle
+bullet.shape("bullet.gif")
+bullet.penup()
 moveShipBy = 0
-
 #game loop that redraws our screen each time the loop executes
 while True:
     
     spaceship.forward(moveShipBy)
-    
+
+        if bullet.isvisible():
+            bullet.setheading(90)
+            bullet.forward(25)
 
     if spaceship.xcor() > 325:
         moveShipBy = 0
